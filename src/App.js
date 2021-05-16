892081fce4f1dd9ecdb91719d7f294c48b7580ab
Tomomi-Ishikawa <68460000+Tomomi-Ishikawa@users.noreply.github.com>
2021-05-16 21:16:13 +0900

    root set1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // react-router-dom -> ルーティングを有効にする
-import { BrowserRouter } from "react-router-dom";
+// Link → aタグみたいなもの
+// Switch → ルート設定
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
@@ -9,11 +11,29 @@ import "./styles.css";
 export default function App() {
   return (
     <BrowserRouter>
+      {/* <BrowserRouter> で囲わないとルーティングされない！ */}
       <div className="App">
-        <Home />
-        <Page1 />
-        <Page2 />
+        <Link to="/">HOME</Link>
+        <br />
+        {/* to = "/" でHOMEに戻るパス */}
+        <Link to="/page1">page1</Link>
+        <br />
+        <Link to="/page2">page2</Link>
+        {/* <Home /> */}
       </div>
+      <Switch>
+        <Route exact path="/">
+          {/* path="/"だとRoute配下のものは<Home />が表示される */}
+          {/* 完全一致以外は対象にしない場合は exact を使う */}
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
