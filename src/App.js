f7d6707247697621deaf817071eb23601099b2bc
Tomomi-Ishikawa <68460000+Tomomi-Ishikawa@users.noreply.github.com>
2021-05-16 20:23:26 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// react-router-dom -> ルーティングを有効にする
+import { BrowserRouter } from "react-router-dom";
+
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Home />
+        <Page1 />
+        <Page2 />
+      </div>
+    </BrowserRouter>
+  );
+}
