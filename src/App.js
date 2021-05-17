0a78c88f18a04318cf6a05e37f586e42dbc019bd
Tomomi-Ishikawa <68460000+Tomomi-Ishikawa@users.noreply.github.com>
2021-05-17 23:54:17 +0900

    details

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
 import "./styles.css";
 
@@ -27,9 +29,41 @@ export default function App() {
           {/* 完全一致以外は対象にしない場合は exact を使う */}
           <Home />
         </Route>
-        <Route path="/page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              {/*  
+          render={() => (
+            <Switch> 
+            {console.log(props)}
+              >>>
+            defaltで以下の値が入ってる。
+            {history: Object, location: Object, match: Object, staticContext: undefined} etc
+
+            match: Object
+              path: "/page1"
+              url: "/page1"
+              isExact: true
+              params: Object
+          */}
+              {console.log(url)}
+              {/* {} で、囲うこと */}
+              {/* <Route exact path="/page1"> */}
+              <Route exact path={url}>
+                {/* テンプレート文字列 { `${}` }*/}
+                <Page1 />
+              </Route>
+              {/* <Route path="/page1/detailA"> */}
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
         <Route path="/page2">
           <Page2 />
         </Route>
